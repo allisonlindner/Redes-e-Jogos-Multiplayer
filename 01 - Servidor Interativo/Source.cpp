0badd49dd91952ea57fb1095537baec183e36051
Allison Lindner <allisonlindner@MacBook-Pro-de-Allison.local>
2015-08-22 12:40:03 -0300

    Configurando o primeiro socket

--- 01 - Servidor Interativo/Source.cpp
+++ 01 - Servidor Interativo/Source.cpp
@@ -3,5 +3,30 @@
 
 int main() {
 
+	WSADATA wsaData;
+
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET principalSocket;
+	principalSocket = socket(	AF_INET,
+								SOCK_STREAM,
+								IPPROTO_TCP		);
+
+	if (principalSocket == INVALID_SOCKET) {
+		std::cout << "Ocorreu um erro ao criar um socket";
+		return 1;
+	}
+
+	closesocket(principalSocket);
+
+	sockaddr_in enderecoServidor;
+	enderecoServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+	enderecoServidor.sin_family = AF_INET;
+	enderecoServidor.sin_port = htons(9999);
+
+	bind(principalSocket, (SOCKADDR*)&enderecoServidor, sizeof(sockaddr_in));
+
+	WSACleanup();
+
 	return 0;
 }
